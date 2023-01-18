32c22e06b80880ba1dc3087892234784c86562d8
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-01-18 11:24:46 +0200

    Adding Problem & Solution 6

---
+++ 1 Month Preparation Kit/JS/06-FlippingBits.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'flippingBits' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+function flippingBits(n) {
+    // Write your code here
+    
+    /* 
+    // Using JS Bitwise
+    
+    // reversing from positive to negative
+    let reverse = ~n;
+    
+    // example of reversing the 0s with 1s
+    (reverse >>> 0).toString(2);
+    
+    // the number that the reversed 0s and 1s represent
+    (reverse >>> 0)
+    
+    */ 
+    
+    return ~n >>> 0;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = flippingBits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
