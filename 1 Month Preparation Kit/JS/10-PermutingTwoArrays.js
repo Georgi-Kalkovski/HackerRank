a4b22a8c9ed9b57fb84f7bada453f37c201644e9
Georgi Kalkovski <54138918+Georgi-Kalkovski@users.noreply.github.com>
2023-01-18 15:31:42 +0200

    Adding Problems & Solutions 8-10

---
+++ 1 Month Preparation Kit/JS/10-PermutingTwoArrays.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
+
+function twoArrays(k, A, B) {
+    // Write your code here
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => a - b).reverse();
+    let isTrue = true;
+
+    for (let i = 0; i < A.length - 1; i++) {
+        if (A[i] + B[i] < k) {
+            isTrue = false;
+        }
+    }
+
+    if (isTrue) { return 'YES'; }
+    else { return 'NO'; }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine().trim(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const k = parseInt(firstMultipleInput[1], 10);
+
+        const A = readLine().replace(/\s+$/g, '').split(' ').map(ATemp => parseInt(ATemp, 10));
+
+        const B = readLine().replace(/\s+$/g, '').split(' ').map(BTemp => parseInt(BTemp, 10));
+
+        const result = twoArrays(k, A, B);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
